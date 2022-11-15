2968817e66f5d76f27e58d76947bc25fc34b6834
JesperKYH <jesper.pettersson@student.kyh.se>
2022-11-15 21:30:15 +0100

    Added home page content

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -10,5 +10,5 @@ export default component$(() => {
 })
 
 export const head: DocumentHead = {
-    title: 'About',
+    title: 'Jesper Pettersson | About',
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,26 @@
-import { component$ } from '@builder.io/qwik'
+import { component$, useStylesScoped$ } from '@builder.io/qwik'
 import type { DocumentHead } from '@builder.io/qwik-city'
+import globalStyles from './home.less'
+import profilePic from '../pictures/jesper_profil_fixad.jpeg'
 
 export default component$(() => {
+    useStylesScoped$(globalStyles)
+
     return (
-        <div>
-            <h1>Jesper</h1>
+        <div className="home-wrapper">
+            <h1>Hello!</h1>
+            <div className="text-wrapper">
+                My name is <span className="name-span">Jesper Pettersson</span>{' '}
+                and I'm a Front-End Developer! I love coding and learn new
+                things at the same time. I've been a front-end dev for about 2
+                years and counting and it's the best choice I've made in my
+                life.
+            </div>
+            <img className="profile-pic" src={profilePic} />
         </div>
     )
 })
 
 export const head: DocumentHead = {
-    title: 'Jesper Pettersson',
+    title: 'Jesper Pettersson | home',
 }
